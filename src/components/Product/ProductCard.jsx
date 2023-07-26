d5ff1a8d1aa733c0be120121f73a041473fe86ce
RampriyaKarthick <118220230+RampriyaKarthick@users.noreply.github.com>
2023-07-26 08:06:57 +0200

    test frontend

--- src/components/Product/ProductCard.jsx
+++ src/components/Product/ProductCard.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
-import axios from "axios";
+import axios from "axios"
 import {
   AiFillHeart,
   AiOutlineEye,
